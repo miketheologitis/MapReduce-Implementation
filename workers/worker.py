9095913d85c94e3d38b5bc778f294bd7e0e84732
miketheologitis <miketh@outlook.com.gr>
2023-05-08 14:30:24 +0300

    flask-start

--- workers/worker.py
+++ workers/worker.py
@@ -1,1 +1,17 @@
-import flask
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Define the routes and handlers for the worker
+@app.route('/map', methods=['POST'])
+def map_task():
+    # Handle the mapper task
+    pass
+
+@app.route('/reduce', methods=['POST'])
+def reduce_task():
+    # Handle the reducer task
+    pass
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
